cce894bc1556011d164d83a9f8af0a752c1c80b2
dwrigley1 <dwrigley1@ivytech.edu>
2025-09-13 12:36:47 -0400

    ui_examples addition

--- Frontend/script.js
+++ Frontend/script.js
@@ -21,3 +21,17 @@ function randomMessage() {
   document.getElementById("game-area").innerText =
     messages[Math.floor(Math.random() * messages.length)];
 }
+
+// BEGIN RANDOM UI ADDITION FUNCTIONS //
+
+// click to win, you lost button //
+function changeText() {
+  var x = document.getElementById("youLost");
+  if (x.innerHTML === "CLICK HERE TO WIN!") {
+    x.innerHTML = "YOU LOST!";
+  } else {
+    x.innerHTML = "beep beep";
+  }
+}
+
+// END RANDOM UI ADDITION FUNCTIONS //
